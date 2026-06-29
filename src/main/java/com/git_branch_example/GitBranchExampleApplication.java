1fffafc60df61fd790d7151c2f441b2dfeda2c43
subhalaxmi <subjalaxmi2003sahoo@gmail.com>
2026-06-29 10:25:31 +0530

    First commit

---
+++ src/main/java/com/git_branch_example/GitBranchExampleApplication.java
@@ -0,0 +1,15 @@
+package com.git_branch_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitBranchExampleApplication {
+
+	public static void main(String[] args) {
+		
+		int a = 1000;
+		SpringApplication.run(GitBranchExampleApplication.class, args);
+	}
+
+}
